690c6e08dd7b224791891a38a232e70535fd75af
Dumskii-Artem <boltushko@gmail.com>
2025-10-27 01:02:14 +0300

    9

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 // import { useState } from 'react'
 import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import ResultPage from './pages/ResultPage.tsx'
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Caritas Frontend</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
