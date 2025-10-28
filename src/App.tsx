b5e760d40548ba042874f814d95e3f2f2533475b
Dumskii-Artem <boltushko@gmail.com>
2025-10-29 01:07:04 +0300

    12

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 // src\App.tsx
 
-import { lazy, Suspense } from 'react';
+import { Suspense } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ErrorBoundary from './ui/ErrorBoundary';
 import FallbackErrorView from './ui/FallbackErrorView';
-import { ROUTES } from './lib/routes';
-
-// ленивые импорты страниц
-const HomePage = lazy(() => import('./pages/Home'));
-const ResultPage = lazy(() => import('./pages/Result'));
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ErrorBoundary>
-        <Suspense fallback={<FallbackErrorView message="Загрузка…" />}>
-          <Routes>
-            <Route path={ROUTES.home} element={<HomePage />} />
-            <Route path={ROUTES.result} element={<ResultPage />} />
-          </Routes>
-        </Suspense>
-      </ErrorBoundary>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <Suspense fallback={<FallbackErrorView message="Загрузка…" />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
