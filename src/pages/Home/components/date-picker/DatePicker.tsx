d5e1c1f282f327614337dbe7dacffedb877facd5
zanin <yu.v.zanin@samsmu.ru>
2025-10-30 12:48:00 +0400

    feat: add data picker with standart input

---
+++ src/pages/Home/components/date-picker/DatePicker.tsx
@@ -0,0 +1,32 @@
+import './DatePicker.css';
+
+type Props = {
+  value: string;
+  onChange: (value: string) => void;
+  label?: string;
+  required?: boolean;
+};
+
+export default function DatePicker({
+  value,
+  onChange,
+  label,
+  required,
+}: Props) {
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  return (
+    <div className="date-picker">
+      <div className="date-label">
+        {label && <label>{label}</label>}
+        {'\u00A0'}
+        {required && <span className="date-required">*</span>}
+      </div>
+      <div className="date-input">
+        <input type="date" value={value} onChange={handleDateChange} />
+      </div>
+    </div>
+  );
+}
