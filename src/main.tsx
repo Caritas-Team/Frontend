e866819bd84934322efd19176f73f2cc2ea163bc
EvgeniaSkasyrskaya <evgenia.skasyrskaya@yandex.ru>
2025-11-07 10:09:55 +0700

    fix(index.css, main.tsx): переименовать файл index.css в reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src\main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './reset.css';
 import './variables.css';
 import App from './App.tsx';
 
