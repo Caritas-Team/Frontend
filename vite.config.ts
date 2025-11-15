c5fb2b372ad90ff9c7b66277e60e70b0ae084be7
Shadrina Tatiana <ta-nett@mail.ru>
2025-11-15 18:43:43 +0100

    Revert "new version"
    This reverts commit 924f8303842609d8ba2c0088f38d076dbfccab58.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
       '@ui': path.resolve(__dirname, 'src/ui'),
     },
   },
