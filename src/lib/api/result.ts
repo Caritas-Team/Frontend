56da58ab3bacf6cec08572ab3bcb612022d15e94
Sergey Terpugov <TerSV@yandex.ru>
2025-11-17 11:26:18 +0500

    fix(upload): поправить структуру отправляемых данных

--- src/lib/api/result.ts
+++ src/lib/api/result.ts
@@ -2,5 +2,5 @@ import { apiClient } from './apiClient';
 import { API_ENDPOINTS } from './endpoints';
 
 export const getResult = (id: string) => {
-  apiClient.get(`${API_ENDPOINTS.results}/${id}`);
+  return apiClient.get(`${API_ENDPOINTS.results}/${id}`);
 };
