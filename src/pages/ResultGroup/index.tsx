d53479c3830be2d0bf0e5a080775ebb2213cf69f
EvgeniaSkasyrskaya <evgenia.skasyrskaya@yandex.ru>
2025-11-19 15:17:05 +0700

    fix(ResultGroupPage): подключить на страницу с результатом группы 3 компонента LineChart с моковыми наборами данных для секций Языковая и коммуникативная оценка, Инициатива, Коммуникативные функции

--- src/pages/ResultGroup/index.tsx
+++ src/pages/ResultGroup/index.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import styles from './ResultGroupPage.module.css';
 import { Header } from './components/header';
-import { LangCommAssessmentGroup } from './components/langCommAssessmentGroup';
+import { LineChartGroup } from './components/lineChart';
 
 type TChartDataItem = {
   name: string;
@@ -17,10 +17,10 @@ type TChartData = {
   currentDate: string;
 };
 
-const mockLineChartData: TChartData = {
+const mockLineChartData_langSkills: TChartData = {
   data: [
     {
-      name: 'Доинтенциальная коммуникация',
+      name: 'Доинтенциональная коммуникация',
       prevValue: '40',
       currentValue: '60',
     },
@@ -44,11 +44,80 @@ const mockLineChartData: TChartData = {
   currentDate: '2025-05-01',
 };
 
+const mockLineChartData_initiative: TChartData = {
+  data: [
+    {
+      name: 'Доинтенциональная коммуникация',
+      prevValue: '30',
+      currentValue: '50',
+    },
+    {
+      name: 'Протоязык',
+      prevValue: '60',
+      currentValue: '40',
+    },
+    {
+      name: 'Голофраза',
+      prevValue: '20',
+      currentValue: '50',
+    },
+    {
+      name: 'Фраза',
+      prevValue: '50',
+      currentValue: '80',
+    },
+  ],
+  prevDate: '2025-04-15',
+  currentDate: '2025-05-01',
+};
+
+const mockLineChartData_communFunctions: TChartData = {
+  data: [
+    {
+      name: 'Обмен информацией',
+      prevValue: '30',
+      currentValue: '50',
+    },
+    {
+      name: 'Сильное взаимодействие',
+      prevValue: '80',
+      currentValue: '40',
+    },
+    {
+      name: 'Получение желаемого результата',
+      prevValue: '20',
+      currentValue: '50',
+    },
+    {
+      name: 'Контроль',
+      prevValue: '60',
+      currentValue: '80',
+    },
+  ],
+  prevDate: '2025-04-15',
+  currentDate: '2025-05-01',
+};
+
 export const ResultGroupPage: React.FC = () => {
   return (
     <main className={styles.main}>
       <Header></Header>
-      <LangCommAssessmentGroup {...mockLineChartData}></LangCommAssessmentGroup>
+      <LineChartGroup
+        className={styles.language_section}
+        title={'Языковая и коммуникативная оценка'}
+        subtitle={'Уровень применения языковых навыков'}
+        {...mockLineChartData_langSkills}
+      ></LineChartGroup>
+      <LineChartGroup
+        className={styles.initiative_section}
+        title={'Инициатива'}
+        {...mockLineChartData_initiative}
+      ></LineChartGroup>
+      <LineChartGroup
+        className={styles.communicative_section}
+        title={'Коммуникативные функции'}
+        {...mockLineChartData_communFunctions}
+      ></LineChartGroup>
     </main>
   );
 };
