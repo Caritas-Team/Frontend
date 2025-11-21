68bb4a02e3749f4c12c6591781aa1a5f75dfcb36
Dmitry Loshakov <loshakovdmitry@yandex.ru>
2025-11-21 10:22:44 +0300

    feat_choice_leader_2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
 // src\pages\Home\index.tsx
 
+import styles from './home.module.css';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../lib/routes';
 import { DatePicker } from './components/date-picker';
 import { useState } from 'react';
 import { Header } from '../ResultGroup/components/header';
-/*import { Loader } from './components/loader/';*/
-/*import { Loader_2 } from './components/loader/';*/
-/*import { Loader_3 } from './components/loader/';*/
-import { CustomLoader } from './components/loader/';
+import { Loader_3 } from './components/loader/';
 
 export default function HomePage() {
   const [formingDate, setFormingDate] = useState<string>('');
@@ -16,38 +14,43 @@ export default function HomePage() {
 
   return (
     <section style={{ padding: '2rem' }}>
-      <Header></Header>
+      <div
+        className={
+          openLoader
+            ? `${styles.pageContent} ${styles.pageContentWithLoader}`
+            : styles.pageContent
+        }
+      >
+        <Header></Header>
 
-      <form onSubmit={e => e.preventDefault()}>
-        <input type="text" placeholder="Введите данные" />
-        <button type="submit">Отправить</button>
-      </form>
+        <form onSubmit={e => e.preventDefault()}>
+          <input type="text" placeholder="Введите данные" />
+          <button type="submit">Отправить</button>
+        </form>
 
-      <p style={{ marginTop: '1rem' }}>
-        <Link to={ROUTES.result}>Перейти к результату</Link>
-      </p>
+        <p style={{ marginTop: '1rem' }}>
+          <Link to={ROUTES.result}>Перейти к результату</Link>
+        </p>
 
-      <p style={{ marginTop: '1rem' }}>
-        <Link to={ROUTES.result_group}>Перейти к групповому результату</Link>
-      </p>
+        <p style={{ marginTop: '1rem' }}>
+          <Link to={ROUTES.result_group}>Перейти к групповому результату</Link>
+        </p>
 
-      <DatePicker
-        value={formingDate}
-        onChange={setFormingDate}
-        label="Дата заполнения"
-        required={true}
-        // error="Данные отсутствуют в системе, проверьте правильность ввода или сообщите об ошибке"
-      />
-      <br />
-      <br />
-      <button type="button" onClick={() => setOpenLoader(true)}>
-        Показать лоадер
-      </button>
+        <DatePicker
+          value={formingDate}
+          onChange={setFormingDate}
+          label="Дата заполнения"
+          required={true}
+          // error="Данные отсутствуют в системе, проверьте правильность ввода или сообщите об ошибке"
+        />
+        <br />
+        <br />
+        <button type="button" onClick={() => setOpenLoader(true)}>
+          Показать лоадер
+        </button>
+      </div>
       {openLoader && (
-        /*<Loader openLoader={openLoader} doClose={() => setOpenLoader(false)} /> */
-        /*<Loader_2 openLoader={openLoader} doClose={() => setOpenLoader(false)} /> */
-        /*<Loader_3 openLoader={openLoader} doClose={() => setOpenLoader(false)} /> */
-        <CustomLoader
+        <Loader_3
           openLoader={openLoader}
           doClose={() => setOpenLoader(false)}
         />
