cbac4844a0c21587ccf1816a484033834618cef8
Dumskii-Artem <boltushko@gmail.com>
2025-11-21 17:48:14 +0300

    add section to group result

--- src/pages/ResultGroup/index.tsx
+++ src/pages/ResultGroup/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import styles from './ResultGroupPage.module.css';
 import { Header } from './components/header';
 import { LangCommAssessmentGroup } from './components/langCommAssessmentGroup';
+import { CheckSection } from '../Result/components/checkSection';
 
 type TChartDataItem = {
   name: string;
@@ -48,6 +49,17 @@ export const ResultGroupPage: React.FC = () => {
   return (
     <main className={styles.main}>
       <Header></Header>
+      <CheckSection
+        date1="15 Апр. 2025"
+        formed1={20}
+        initiative1={35}
+        frequency1={50}
+        date2="1 Мая 2025"
+        formed2={90}
+        initiative2={20}
+        frequency2={55}
+        description="Прилетит, вдруг, волшебник"
+      />
       <LangCommAssessmentGroup {...mockLineChartData}></LangCommAssessmentGroup>
     </main>
   );
