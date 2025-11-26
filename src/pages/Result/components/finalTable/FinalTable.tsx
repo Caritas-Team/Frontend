e08b7aaa77de1878bae5f443a2283f3053debe37
Tanya Hohlova <tanya.hohlova10@mail.ru>
2025-11-26 16:28:21 +0300

    исправоение таблици

--- src/pages/Result/components/finalTable/FinalTable.tsx
+++ src/pages/Result/components/finalTable/FinalTable.tsx
@@ -20,63 +20,58 @@ export const FinalTable: React.FC<FinalTableProps> = ({
   return (
     <section className={styles.finalTable}>
       <h3 className={styles.caption}>Итоговая таблица</h3>
-      <table className={styles.table}>
-        <thead className={styles.header}>
-          <tr className={styles.headerRow}>
-            <td
-              className={`${styles.headerCell} ${styles.headerCellParameters}`}
-            >
-              Параметры:
-            </td>
-            <td className={styles.headerCell}>%/кол-во:</td>
-          </tr>
-        </thead>
-        <tbody className={styles.body}>
-          <tr className={styles.row}>
-            <td className={styles.parameterCell}>Уровни языкового развития</td>
-            <td className={styles.valueCell}>
-              <div className={styles.boxDelta}>
-                <Delta text={languageDevelopmentLevels} up={true} />
-              </div>
-            </td>
-          </tr>
-          <tr className={styles.row}>
-            <td className={styles.parameterCell}>Инициатива</td>
-            <td className={styles.valueCell}>
-              <div className={styles.boxDelta}>
-                <Delta text={communicationInitiative} up={true} />
-              </div>
-            </td>
-          </tr>
-          <tr className={styles.row}>
-            <td className={styles.parameterCell}>
-              Развитие коммуникативных функций
-            </td>
-            <td className={styles.valueCell}>
-              <div className={styles.boxDelta}>
-                <span className={styles.wrapperWarning}></span>
-                <Delta text={communicativeFunctionsProgress} up={false} />
-              </div>
-            </td>
-          </tr>
-          <tr className={styles.row}>
-            <td className={styles.parameterCell}>Словарный запас</td>
-            <td className={styles.valueCell}>
-              <div className={styles.boxDelta}>
-                <Delta text={vocabularyLevel} up={true} />
-              </div>
-            </td>
-          </tr>
-          <tr className={styles.row}>
-            <td className={styles.parameterCell}>Устные слова</td>
-            <td className={styles.valueCell}>
-              <div className={styles.boxDelta}>
-                <Delta text={spokenWordsCount} up={false} />
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles.gridContainer}>
+        {/* Заголовок */}
+        <div className={`${styles.gridHeader} ${styles.gridHeaderParameters}`}>
+          Параметры:
+        </div>
+        <div className={`${styles.gridHeader} ${styles.gridHeaderValue}`}>
+          %/кол-во:
+        </div>
+
+        {/* Строки данных */}
+        <div className={styles.gridParameter}>Уровни языкового развития</div>
+        <div className={styles.gridValue}>
+          <div className={styles.boxDelta}>
+            {/* <span className={styles.wrapperWarning}></span> */}
+            <Delta text={languageDevelopmentLevels} up={true} />
+          </div>
+        </div>
+
+        <div className={styles.gridParameter}>Инициатива</div>
+        <div className={styles.gridValue}>
+          <div className={styles.boxDelta}>
+            {/* <span className={styles.wrapperWarning}></span> */}
+            <Delta text={communicationInitiative} up={true} />
+          </div>
+        </div>
+
+        <div className={styles.gridParameter}>
+          Развитие коммуникативных функций
+        </div>
+        <div className={styles.gridValue}>
+          <div className={styles.boxDelta}>
+            <span className={styles.wrapperWarning}></span>
+            <Delta text={communicativeFunctionsProgress} up={false} />
+          </div>
+        </div>
+
+        <div className={styles.gridParameter}>Словарный запас</div>
+        <div className={styles.gridValue}>
+          <div className={styles.boxDelta}>
+            {/* <span className={styles.wrapperWarning}></span> */}
+            <Delta text={vocabularyLevel} up={true} />
+          </div>
+        </div>
+
+        <div className={styles.gridParameter}>Устные слова</div>
+        <div className={styles.gridValue}>
+          <div className={styles.boxDelta}>
+            {/* <span className={styles.wrapperWarning}></span> */}
+            <Delta text={spokenWordsCount} up={false} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
