8d39e81f333772b76d094577983eb3e3e675164f
EvgeniaSkasyrskaya <evgenia.skasyrskaya@yandex.ru>
2025-12-02 17:56:06 +0700

    fix(utils.test.ts): сделать дату для тестирования функции calculateAge вычисляемой от текущей даты, добавить случаи в тестах

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -6,18 +6,40 @@ import {
   makeShortName,
 } from './utils';
 
+const createDateForTest = (yearsNumber = 0): string => {
+  const today = new Date();
+  const year = today.getFullYear() - yearsNumber;
+  const month =
+    today.getMonth() > 8
+      ? today.getMonth() + 1
+      : '0'.concat(String(today.getMonth() + 1));
+  const day =
+    today.getDate() > 9 ? today.getDate() : '0'.concat(String(today.getDate()));
+  return `${year}-${month}-${day}`;
+};
+
 describe('тестирование функции проверки валидности даты', () => {
   test('проверка корректной даты', () => {
     const result = isValidDate('2015-04-03');
     expect(result).toBe(true);
   });
 
-  test('проверка некорректной (несуществующей) даты', () => {
+  test('проверка корректной даты - начало года', () => {
+    const result = isValidDate('2025-01-01');
+    expect(result).toBe(true);
+  });
+
+  test('проверка корректной даты - конец года', () => {
+    const result = isValidDate('2025-12-31');
+    expect(result).toBe(true);
+  });
+
+  test('проверка некорректной (несуществующей) даты - 15-ый месяц', () => {
     const result = isValidDate('2015-15-14');
     expect(result).toBe(false);
   });
 
-  test('проверка некорректной (несуществующей) даты', () => {
+  test('проверка некорректной (несуществующей) даты - 30 февраля', () => {
     const result = isValidDate('2015-02-30');
     expect(result).toBe(false);
   });
@@ -40,22 +62,22 @@ describe('тестирование функции проверки валидн�
 
 describe('тестирование функции вычисления возраста', () => {
   test('вычисление корректного возраста с окончанием "лет"', () => {
-    const result = calculateAge('2015-04-03');
+    const result = calculateAge(createDateForTest(10));
     expect(result).toBe('10 лет');
   });
 
   test('вычисление корректного возраста с окончанием "год"', () => {
-    const result = calculateAge('2004-10-14');
+    const result = calculateAge(createDateForTest(21));
     expect(result).toBe('21 год');
   });
 
   test('вычисление корректного возраста с окончанием "года"', () => {
-    const result = calculateAge('2021-08-22');
+    const result = calculateAge(createDateForTest(4));
     expect(result).toBe('4 года');
   });
 
   test('дата рождения позже сегодняшней', () => {
-    const result = calculateAge('2026-08-22');
+    const result = calculateAge(createDateForTest(-1));
     expect(result).toBe('дата рождения превышает текущую');
   });
 
@@ -106,5 +128,10 @@ describe('тестирование функции сокращения стро�
   test('сокращение строки, случай 3', () => {
     const result = makeShortName('Инсулинозависимый сахарный диабет', 6, 1);
     expect(result).toBe('И. с. диабет');
+  });
+
+  test('сокращение строки, случай 4 - пустая строка', () => {
+    const result = makeShortName('', 10, 10);
+    expect(result).toBe('');
   });
 });
