ea693800a9696ab9c32246bdcfb9d5f4ae2297da
Sergey Terpugov <TerSV@yandex.ru>
2025-12-04 10:54:30 +0500

    feat(FormPage): собрать страницу из компонентов

--- src/pages/Home/components/layout/layout.tsx
+++ src/pages/Home/components/layout/layout.tsx
@@ -3,8 +3,30 @@ import { MainWrapper } from '../main-wrapper';
 import { Logo } from '@/ui/logo';
 import { TitleSection } from '../titleSection';
 import { DatePicker } from '../date-picker';
+import { useEffect, useState } from 'react';
+import { InstructionPopup } from '../InstructionPopup';
+import { MainBlockForm } from '../MainBlockForm';
 
 export function Layout() {
+  const [selectDate, setSelectDate] = useState('');
+  const [isOpenPopup, setIsOpenPopup] = useState(false);
+
+  const handleDate = (newDate: string) => {
+    setSelectDate(newDate);
+  };
+
+  const handleTargetPopup = () => {
+    setIsOpenPopup(prev => !prev);
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (!storedUser) {
+      localStorage.setItem('user', JSON.stringify({ popupSeen: true }));
+      setIsOpenPopup(true);
+    }
+  }, []);
+
   return (
     <MainWrapper>
       <header className={styles.header}>
@@ -16,14 +38,17 @@ export function Layout() {
         </div>
         <div className={styles.dateBlock}>
           <DatePicker
-            value={'selectDate'}
-            onChange={() => {}}
+            value={selectDate}
+            onChange={handleDate}
             label={'Дата заполнения'}
             required={true}
           />
         </div>
       </header>
-      <main className={styles.main}>{'Main'}</main>
+      <main className={styles.main}>
+        <MainBlockForm openPopup={handleTargetPopup} />
+      </main>
+      <InstructionPopup isOpen={isOpenPopup} doClose={handleTargetPopup} />
     </MainWrapper>
   );
 }
